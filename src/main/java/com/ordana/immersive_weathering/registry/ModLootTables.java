fc4b6e87722b1afa9a1376096860ac0408f2bd79
Mitchell Skaggs <skaggsm333@gmail.com>
2022-06-14 15:12:20 -0500

    Replace Loot API v1 with v2

--- src/main/java/com/ordana/immersive_weathering/registry/ModLootTables.java
+++ src/main/java/com/ordana/immersive_weathering/registry/ModLootTables.java
@@ -1,24 +1,16 @@
 package com.ordana.immersive_weathering.registry;
 
 import com.ordana.immersive_weathering.registry.items.ModItems;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
-import net.minecraft.enchantment.Enchantments;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.Items;
-import net.minecraft.loot.condition.DamageSourcePropertiesLootCondition;
-import net.minecraft.loot.condition.MatchToolLootCondition;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.LootingEnchantLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
-import net.minecraft.predicate.NumberRange;
-import net.minecraft.predicate.entity.DamageSourcePredicate;
-import net.minecraft.predicate.item.EnchantmentPredicate;
-import net.minecraft.predicate.item.ItemPredicate;
 import net.minecraft.util.Identifier;
 
 public class ModLootTables {
@@ -39,126 +31,124 @@ public class ModLootTables {
 
     public static void registerLootTables() {
 
-        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, table, setter) ->
-                {
-                    //mobs
-                    if (BLAZE_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            //mobs
+            if (BLAZE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.SOOT)
-                                        .conditionally(RandomChanceLootCondition.builder(0.75f)))
-                                .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 3.0f)).build())
-                                .withFunction(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(1f, 0f)).build());
+                        .with(ItemEntry.builder(ModItems.SOOT)
+                                .conditionally(RandomChanceLootCondition.builder(0.75f)))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 3.0f)).build())
+                        .apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(1f, 0f)).build());
 
-                        table.withPool(poolBuilder.build());
-                    }
+                tableBuilder.pool(poolBuilder);
+            }
 
-                    //loot chests
-                    if (SHIPWRECK_TREASURE_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+            //loot chests
+            if (SHIPWRECK_TREASURE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.ENCHANTED_GOLDEN_MOSS_CLUMP)
-                                        .conditionally(RandomChanceLootCondition.builder(0.5f)))
+                        .with(ItemEntry.builder(ModItems.ENCHANTED_GOLDEN_MOSS_CLUMP)
+                                .conditionally(RandomChanceLootCondition.builder(0.5f)))
 
-                                .with(ItemEntry.builder(ModItems.GOLDEN_MOSS_CLUMP)
-                                        .conditionally(RandomChanceLootCondition.builder(0.75f)))
-                                .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f), false).build());
+                        .with(ItemEntry.builder(ModItems.GOLDEN_MOSS_CLUMP)
+                                .conditionally(RandomChanceLootCondition.builder(0.75f)))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f), false).build());
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (SHIPWRECK_SUPPLY_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (SHIPWRECK_SUPPLY_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.MOSS_CLUMP)
-                                        .conditionally(RandomChanceLootCondition.builder(0.75f)))
-                                .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(3.0f, 5.0f)).build());
+                        .with(ItemEntry.builder(ModItems.MOSS_CLUMP)
+                                .conditionally(RandomChanceLootCondition.builder(0.75f)))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3.0f, 5.0f)).build());
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (BURIED_TREASURE_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (BURIED_TREASURE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.ENCHANTED_GOLDEN_MOSS_CLUMP)
-                                        .conditionally(RandomChanceLootCondition.builder(0.75f)));
+                        .with(ItemEntry.builder(ModItems.ENCHANTED_GOLDEN_MOSS_CLUMP)
+                                .conditionally(RandomChanceLootCondition.builder(0.75f)));
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (NETHER_FORTRESS_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (NETHER_FORTRESS_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(Items.NETHER_BRICK)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(5.0f, 11.0f), false).build());
+                        .with(ItemEntry.builder(Items.NETHER_BRICK)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(5.0f, 11.0f), false).build());
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (FARMER_GIFT_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (FARMER_GIFT_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.MULCH_BLOCK)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .with(ItemEntry.builder(ModItems.HUMUS)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 5.0f), false).build());
+                        .with(ItemEntry.builder(ModItems.MULCH_BLOCK)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .with(ItemEntry.builder(ModItems.HUMUS)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 5.0f), false).build());
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (ARMORER_GIFT_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (ARMORER_GIFT_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.STEEL_WOOL)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)));
+                        .with(ItemEntry.builder(ModItems.STEEL_WOOL)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)));
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (TOOLSMITH_GIFT_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (TOOLSMITH_GIFT_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(Items.FLINT_AND_STEEL))
-                                .with(ItemEntry.builder(ModItems.STEEL_WOOL))
-                                .with(ItemEntry.builder(Items.HONEYCOMB))
-                                .with(ItemEntry.builder(Items.SHEARS));
+                        .with(ItemEntry.builder(Items.FLINT_AND_STEEL))
+                        .with(ItemEntry.builder(ModItems.STEEL_WOOL))
+                        .with(ItemEntry.builder(Items.HONEYCOMB))
+                        .with(ItemEntry.builder(Items.SHEARS));
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (WEAPONSMITH_GIFT_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (WEAPONSMITH_GIFT_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.ICE_SICKLE)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)));
+                        .with(ItemEntry.builder(ModItems.ICE_SICKLE)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)));
 
-                        table.withPool(poolBuilder.build());
-                    }
-                    if (MASON_GIFT_ID.equals(id)) {
-                        FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                                .rolls(ConstantLootNumberProvider.create(1))
+                tableBuilder.pool(poolBuilder);
+            }
+            if (MASON_GIFT_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
 
-                                .with(ItemEntry.builder(ModItems.STONE_BRICK)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .with(ItemEntry.builder(ModItems.DEEPSLATE_BRICK)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .with(ItemEntry.builder(ModItems.PLATE_IRON)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .with(ItemEntry.builder(ModItems.CUT_IRON)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .with(ItemEntry.builder(ModItems.VITRIFIED_SAND)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .with(ItemEntry.builder(Items.HONEYCOMB)
-                                        .conditionally(RandomChanceLootCondition.builder(0.25f)))
-                                .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0f, 7.0f), false).build());
+                        .with(ItemEntry.builder(ModItems.STONE_BRICK)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .with(ItemEntry.builder(ModItems.DEEPSLATE_BRICK)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .with(ItemEntry.builder(ModItems.PLATE_IRON)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .with(ItemEntry.builder(ModItems.CUT_IRON)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .with(ItemEntry.builder(ModItems.VITRIFIED_SAND)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .with(ItemEntry.builder(Items.HONEYCOMB)
+                                .conditionally(RandomChanceLootCondition.builder(0.25f)))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0f, 7.0f), false).build());
 
-                        table.withPool(poolBuilder.build());
-                    }
-                }
-        );
+                tableBuilder.pool(poolBuilder);
+            }
+        });
     }
 }
