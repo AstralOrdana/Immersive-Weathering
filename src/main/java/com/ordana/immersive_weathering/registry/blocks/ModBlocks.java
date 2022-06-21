5baeac86aa384b1eacae9832df8bbaaa83a3cd8f
foul-fortune-feline <foul-fortune-feline@pm.me>
2022-06-21 15:24:35 -0700

    Added compat with Aurora's Decorations and consequently made charred blocks a bit more robust in their return values (instead of crashing if they try to get information from them when they haven't yet been registered, IW will return as though the blocks aren't smoldering, i.e. a default state)

--- src/main/java/com/ordana/immersive_weathering/registry/blocks/ModBlocks.java
+++ src/main/java/com/ordana/immersive_weathering/registry/blocks/ModBlocks.java
@@ -218,7 +218,13 @@ public class ModBlocks {
     public static final Block WAXED_RUSTED_IRON_BARS = new WaxedBarsBlock(FabricBlockSettings.of(Material.METAL).requiresTool().strength(5f, 6f).sounds(BlockSoundGroup.METAL).nonOpaque());
 
     private static ToIntFunction<BlockState> createLightLevelFromSmolderingBlockState(int litLevel) {
-        return (state) -> (Boolean)state.get(CharredBlock.SMOLDERING) ? litLevel : 0;
+        return (state) -> {
+            try {
+                return (Boolean)state.get(CharredBlock.SMOLDERING) ? litLevel : 0;
+            } catch (IllegalArgumentException e) {
+                return 0;
+            }
+        };
     }
 
     private static ToIntFunction<BlockState> createLightLevelFromMoltenBlockState(int litLevel) {
@@ -230,7 +236,11 @@ public class ModBlocks {
     }
 
     private static boolean emissiveIfSmoldering(BlockState state, BlockView world, BlockPos pos) {
-        return state.get(CharredBlock.SMOLDERING);
+        try {
+            return state.get(CharredBlock.SMOLDERING);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 
     private static boolean emissiveIfMolten(BlockState state, BlockView world, BlockPos pos) {
