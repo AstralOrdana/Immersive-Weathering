f8fa50205297f257b7d91309d9da88759fa1feda
MehVahdJukaar <matteogozzi1998@gmail.com>
2022-09-16 21:43:22 +0200

    loot

--- common/src/main/java/com/ordana/immersive_weathering/events/ModLootInjects.java
+++ common/src/main/java/com/ordana/immersive_weathering/events/ModLootInjects.java
@@ -1,9 +1,13 @@
 package com.ordana.immersive_weathering.events;
 
+import com.ordana.immersive_weathering.ImmersiveWeathering;
+import net.mehvahdjukaar.moonlight.api.platform.ForgeHelper;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.entries.LootTableReference;
 
+import java.util.Locale;
 import java.util.function.Consumer;
 
 public class ModLootInjects {
@@ -11,6 +15,8 @@ public class ModLootInjects {
     public static void onLootInject(LootTables lootManager, ResourceLocation name, Consumer<LootPool.Builder> builderConsumer) {
 
         if(name.equals(new ResourceLocation("something"))){
+
+            //this is how you add an hardcoded one
               LootPool.Builder poolBuilder = LootPool.lootPool();
                  poolBuilder.setRolls(ConstantValue.exactly(1))
 
@@ -22,6 +28,16 @@ public class ModLootInjects {
             table.withPool(poolBuilder.build());
 
             builderConsumer.accept(poolBuilder);
+
+
+            //this is a json one
+
+            String id = type.toString().toLowerCase(Locale.ROOT) + "_" + name;
+            LootPool.Builder pool = LootPool.lootPool();
+
+                  pool  .add(LootTableReference.lootTableReference(ImmersiveWeathering.res("inject/" + id)));
+            //ForgeHelper.setPoolName(pool, "supp_" + name); TODO: add
+            consumer.accept(pool);
         }
     }
 }
