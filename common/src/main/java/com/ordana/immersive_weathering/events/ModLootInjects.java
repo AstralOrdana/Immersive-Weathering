087755fe4ad77c7a87772cc8cce883c8a01009b1
Ordana <astralordana@gmail.com>
2022-09-16 16:53:29 -0500

    new loot table setup

--- common/src/main/java/com/ordana/immersive_weathering/events/ModLootInjects.java
+++ common/src/main/java/com/ordana/immersive_weathering/events/ModLootInjects.java
@@ -1,11 +1,19 @@
 package com.ordana.immersive_weathering.events;
 
 import com.ordana.immersive_weathering.ImmersiveWeathering;
+import com.ordana.immersive_weathering.reg.ModBlocks;
 import net.mehvahdjukaar.moonlight.api.platform.ForgeHelper;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.entries.LootTableReference;
+import net.minecraft.world.level.storage.loot.functions.LootingEnchantFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -14,30 +22,13 @@ public class ModLootInjects {
 
     public static void onLootInject(LootTables lootManager, ResourceLocation name, Consumer<LootPool.Builder> builderConsumer) {
 
-        if(name.equals(new ResourceLocation("something"))){
+        if(name.equals(EntityType.BLAZE.getDefaultLootTable())) {
 
-            //this is how you add an hardcoded one
-              LootPool.Builder poolBuilder = LootPool.lootPool();
-                 poolBuilder.setRolls(ConstantValue.exactly(1))
-
-                    .add(LootItem.lootTableItem(ModBlocks.SOOT.get().asItem())
-                            .when(LootItemRandomChanceCondition.randomChance(0.75f)))
-                    .withFunction(SetItemCountFunction.setCount(UniformGenerator.between(1.0f, 3.0f)).build())
-                    .withFunction(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(1f, 0f)).build());
-
-            table.withPool(poolBuilder.build());
-
-            builderConsumer.accept(poolBuilder);
-
-
-            //this is a json one
-
-            String id = type.toString().toLowerCase(Locale.ROOT) + "_" + name;
             LootPool.Builder pool = LootPool.lootPool();
-
-                  pool  .add(LootTableReference.lootTableReference(ImmersiveWeathering.res("inject/" + id)));
+            pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/blaze_ash")));
             //ForgeHelper.setPoolName(pool, "supp_" + name); TODO: add
-            consumer.accept(pool);
+            builderConsumer.accept(pool);
         }
+
     }
 }
