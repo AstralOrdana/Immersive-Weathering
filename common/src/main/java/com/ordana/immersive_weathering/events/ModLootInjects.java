0beb77d564ad0e03885ae477c9a836bbc02f8bdd
Ordana <astralordana@gmail.com>
2022-09-16 19:02:07 -0500

    data driven loot table injects

--- common/src/main/java/com/ordana/immersive_weathering/events/ModLootInjects.java
+++ common/src/main/java/com/ordana/immersive_weathering/events/ModLootInjects.java
@@ -23,14 +23,96 @@ public class ModLootInjects {
                 ForgeHelper.setPoolName(pool, "IW_" + id);
                 builderConsumer.accept(pool);
             }
+        }
+        if (name.equals(EntityType.PIG.getDefaultLootTable())) {
             {
                 LootPool.Builder pool = LootPool.lootPool();
-                String id = "delete";
+                String id = "pig_tallow";
                 pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
                 ForgeHelper.setPoolName(pool, "IW_" + id);
                 builderConsumer.accept(pool);
             }
         }
-
+        if (name.equals(EntityType.HOGLIN.getDefaultLootTable())) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "hoglin_tallow";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "chests/nether_bridge"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "nether_fortress";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "chests/buried_treasure"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "buried_treasure";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "chests/shipwreck_supply"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "shipwreck_supply";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "chests/shipwreck_treasure"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "shipwreck_treasure";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "gameplay/hero_of_the_village/farmer_gift"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "farmer_gift";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "gameplay/hero_of_the_village/mason_gift"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "mason_gift";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "gameplay/hero_of_the_village/toolsmith_gift"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "toolsmith_gift";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
+        if (name.equals(new ResourceLocation("minecraft", "gameplay/hero_of_the_village/weaponsmith_gift"))) {
+            {
+                LootPool.Builder pool = LootPool.lootPool();
+                String id = "weaponsmith_gift";
+                pool.add(LootTableReference.lootTableReference(ImmersiveWeathering.res("injects/" + id)));
+                ForgeHelper.setPoolName(pool, "IW_" + id);
+                builderConsumer.accept(pool);
+            }
+        }
     }
 }
