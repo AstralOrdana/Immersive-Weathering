df7c40109b6c2fc82abacf97f209f15aa2e52a4f
iGalaxyYT <contact@igalaxy.dev>
2022-09-25 20:51:57 -0700

    fix: server config crash

--- common/src/main/java/com/ordana/immersive_weathering/configs/ClientConfigs.java
+++ common/src/main/java/com/ordana/immersive_weathering/configs/ClientConfigs.java
@@ -16,7 +16,6 @@ public class ClientConfigs {
 
     public static Supplier<Boolean> LEAF_DECAY_PARTICLES;
     public static Supplier<Boolean> FALLING_LEAF_PARTICLES;
-    public static Supplier<Boolean> CREATIVE_TAB;
 
     public static void init() {
         ConfigBuilder builder = ConfigBuilder.create(ImmersiveWeathering.res("client"), ConfigType.CLIENT);
@@ -24,7 +23,6 @@ public class ClientConfigs {
         builder.push("general");
         LEAF_DECAY_PARTICLES = builder.define("leaves_decay_particles", true);
         FALLING_LEAF_PARTICLES = builder.define("falling_leaf_particles", true);
-        CREATIVE_TAB = builder.define("creative_tab", false);
         builder.pop();
 
         CLIENT_SPEC = builder.buildAndRegister();
