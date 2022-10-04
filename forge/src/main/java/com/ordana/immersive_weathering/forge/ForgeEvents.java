a297f65315c4fcc3b87b9d452c04cee93a808f4e
MehVahdJukaar <matteogozzi1998@gmail.com>
2022-10-04 21:41:07 +0200

    mc log spam for optional tiles begone

--- forge/src/main/java/com/ordana/immersive_weathering/forge/ForgeEvents.java
+++ forge/src/main/java/com/ordana/immersive_weathering/forge/ForgeEvents.java
@@ -6,6 +6,7 @@ import com.ordana.immersive_weathering.data.block_growths.BlockGrowthHandler;
 import com.ordana.immersive_weathering.data.fluid_generators.FluidGeneratorsHandler;
 import com.ordana.immersive_weathering.events.ModLootInjects;
 import com.ordana.immersive_weathering.mixins.EatBlockGoalMixin;
+import net.mehvahdjukaar.selene.Selene;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.ai.goal.EatBlockGoal;
 import net.minecraftforge.event.LootTableLoadEvent;
