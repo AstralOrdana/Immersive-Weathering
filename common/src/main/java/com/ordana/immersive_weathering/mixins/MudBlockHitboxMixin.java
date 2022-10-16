3056f4e61c71c566c4ae19c4be72351353045df1
MehVahdJukaar <matteogozzi1998@gmail.com>
2022-10-16 18:50:07 +0200

    mudxin

--- common/src/main/java/com/ordana/immersive_weathering/mixins/MudBlockHitboxMixin.java
+++ common/src/main/java/com/ordana/immersive_weathering/mixins/MudBlockHitboxMixin.java
@@ -3,6 +3,7 @@ package com.ordana.immersive_weathering.mixins;
 import com.ordana.immersive_weathering.entities.FallingLayerEntity;
 import com.ordana.immersive_weathering.entities.FallingPropaguleEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.MudBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -20,7 +21,7 @@ import javax.swing.text.html.BlockView;
 public abstract class MudBlockHitboxMixin {
 
     @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
-    public void getCollisionShape(BlockState state, BlockView world, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir) {
+    public void getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir) {
         if (context instanceof EntityCollisionContext c) {
             var e = c.getEntity();
             if (e instanceof FallingPropaguleEntity || e instanceof FallingLayerEntity) {
