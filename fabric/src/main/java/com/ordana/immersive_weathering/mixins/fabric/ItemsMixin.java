654306340859d003cfa988463a60f817173cc005
MehVahdJukaar <matteogozzi1998@gmail.com>
2022-10-16 19:04:42 +0200

    mixitem

---
+++ fabric/src/main/java/com/ordana/immersive_weathering/mixins/fabric/ItemsMixin.java
@@ -0,0 +1,33 @@
+package com.ordana.immersive_weathering.mixins.fabric;
+
+import com.ordana.immersive_weathering.fabric.CeilingAndWallBlockItem;
+import com.ordana.immersive_weathering.reg.ModBlocks;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Items.class)
+public abstract class ItemsMixin {
+
+    @Shadow
+    private static Item registerBlock(BlockItem item) {
+        return null;
+    }
+
+    @Inject(method = "registerBlock(Lnet/minecraft/world/level/block/Block;Lnet/minecraft/world/item/CreativeModeTab;)Lnet/minecraft/world/item/Item;",
+            at = @At("HEAD"), cancellable = true)
+    private static void addRoot(Block block, CreativeModeTab itemGroup, CallbackInfoReturnable<Item> cir) {
+        if (block == Blocks.HANGING_ROOTS) {
+            cir.setReturnValue(registerBlock(new CeilingAndWallBlockItem(block, ModBlocks.HANGING_ROOTS_WALL.get(),
+                    new Item.Properties().tab(itemGroup))));
+        }
+    }
+}
